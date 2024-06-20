b530fd58c55e779dced9ba6270cfd506766c7276
dangchien2003 <chienboy03@gmail.com>
2024-06-20 16:49:32 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/ticket_manager/Ticket_manager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ticket_manager;
+
+/**
+ *
+ * @author chien
+ */
+public class Ticket_manager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
